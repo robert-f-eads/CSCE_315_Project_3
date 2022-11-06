641edae45fb36574eeca9b68710199b29f2e7f10
alexiahassan <alexiahassan@tamu.edu>
2022-11-06 13:55:33 -0600

    Searchbar Component Created

---
+++ src/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './SideBar.css'
+
+export default function SearchBar() {
+    return (
+        <div class="row" style={{ "align-content": "left !important", "padding-bottom": "25px" }}>
+            <div class="col">
+                <div class="search">
+                    <input type="text" class="form-control" placeholder="Search for Item"></input>
+                    <button class="btn btn-primary">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
