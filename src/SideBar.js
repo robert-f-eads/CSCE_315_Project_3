44bc317a08598e36fea3f62203f4e39788d95281
alexiahassan <alexiahassan@tamu.edu>
2022-11-06 12:40:48 -0600

    navbar react component

---
+++ src/SideBar.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import './SideBar.css'
+import SmoothieKingLogo from './logo.png'
+
+export default function SideBar() {
+    return (
+        <>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"></link>
+            <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/animatecss/3.5.2/animate.min.css"></link>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"></link>
+            <link href='https://fonts.googleapis.com/css?family=Parisienne' rel='stylesheet'></link>
+
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+            <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js" integrity="sha256-xLD7nhI62fcsEZK2/v8LsBcb4lG7dgULkuXoXB/j91c=" crossorigin="anonymous"></script>
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+            <script src="https://kit.fontawesome.com/0b9f4346c0.js" crossorigin="anonymous"></script>
+            <script>
+                new WOW().init();
+            </script>
+
+
+            <div className=" fakeMain d-flex flex-nowrap" style={{ "background-color": "red" }}>
+                <div className="d-flex flex-column flex-shrink-0 p-3 bg-light" style={{ "width": "280px", "height":"100vh;"}}>
+                    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+                        <div className="container">
+                            <div className="row">
+                                <img alt="Test" src= {SmoothieKingLogo}></img>
+                            </div>
+                        </div>
+                    </a>
+                    <hr></hr>
+
+                     <ul className="nav nav-pills flex-column mb-auto">
+                            <li className="nav-item">
+                                <a href="www.google.com" className="nav-link link-dark" aria-current="page">
+                                    Feel Energized
+                                </a>
+                            </li>
+                            <li>
+                                <a href="www.google.com" className="nav-link link-dark">
+                                    Get Fit
+                                </a>
+                            </li>
+                             <li>
+                                <a href="www.google.com" className="nav-link link-dark">
+                                    Manage Weight
+                                </a>
+                            </li>
+                           <li>
+                                <a href="www.google.com" className="nav-link link-dark">
+                                    Be Well
+                                </a>
+                            </li>
+                            <li>
+                                <a href="www.google.com" className="nav-link link-dark">Enjoy A Treat</a> 
+                            </li>
+                        </ul>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
