974e18c17d992f593edfc4476f9e84cd930745ad
Robert Eads <robert.f.eads@tamu.edu>
2022-11-07 17:21:23 -0600

    Layed out framework for switching to each page

---
+++ src/viewConnections.jsx
@@ -0,0 +1,13 @@
+import './viewConnections.css';
+
+const ViewSwitch = () => {
+    return (
+        <div class = "button-holder">
+            <a href="https://www.google.com"><button type="button">Manager View</button></a>
+            <button href = "" type="button">Server View</button>
+            <button href = "" type="button">Customer View</button>
+        </div>
+    )
+}
+
+export default ViewSwitch
