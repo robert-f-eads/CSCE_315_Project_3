3bc25b409c3d4147830ac2034c4db2e8e52e9b13
ongEmma <emma258@tamu.edu>
2022-11-08 19:37:40 -0600

    Added fields for customer name and customer id, connected search bar to backend

--- src/server-view/MainPanel.js
+++ src/server-view/MainPanel.js
@@ -3,7 +3,14 @@ import './MainPanel.css'
 import SmoothieKingLogo from './logo2.png'
 import SearchBar from './SearchBar'
 import SideBar from './SideBar'
+import { getProductsByName } from '../databaseConnections/sharedFunctions'
 
+function showProducts(productName) {
+    console.log("Showing products");
+    getProductsByName(productName).then(res => {
+        console.log(res);
+    });
+}
 
 export default function MainPanel() {
     return (
@@ -23,9 +30,7 @@ export default function MainPanel() {
                         })} */}
                     </div>
             // </div>
-            
         // </>
-
     )
 }
 
