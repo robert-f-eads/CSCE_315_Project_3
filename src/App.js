11283a70fcad92f5e28e88b5f852654eaf6f3c5f
ongEmma <emma258@tamu.edu>
2022-11-08 18:33:08 -0600

    Initial commit for server view, added search bar, sidebar, logo, buttons for logout, cancel, and finish and pay, and server name

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import './App.css';
-import { useState } from 'react';
-//import DbApp from './databaseConnections/databaseTesting';
-//import logo from './logo.svg'
-import 'font-awesome/css/font-awesome.min.css';
-import CustomerView from './customer-view/CustomerView.js'
-import SideBar from './server-view/SideBar';
+import DbApp from './databaseConnections/databaseTesting';
+import SearchBar from './server-view/SearchBar';
 import MainPanel from './server-view/MainPanel';
-import ViewSwitch from './landingPage/viewConnections';
-import ManagerViewTester from './managerView/src/managerViewTester';
-import HomeButton from './homeButton';
-import Modifications from './customer-view/Modifications';
-//import Banner from './banner.png'
-
+import SideBar from './server-view/SideBar';
 
 function App() {
   const [pageNum, setPageNum] = useState(0);
 
   return (
     <>
-      {/*
-      {pageNum === 0 && <ViewSwitch setPageNum={setPageNum}/>}
-      {pageNum === 1 && <CustomerView />}
-      {pageNum === 2 && <div id="serverViewContainer">
-        <MainPanel />
-        <SideBar />
-      </div>}
-      {pageNum === 3 && <ManagerViewTester />}
-      {pageNum !== 0 && <HomeButton setPageNum={setPageNum} />}
-      */}
-      <Modifications itemName = "Item Name"></Modifications>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      {/* <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <DbApp/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div> */}
 
+    
+      <div id="allContent">
+        <MainPanel></MainPanel>
+        <SideBar></SideBar>
+        {/* <SearchBar></SearchBar> */}
+
+      </div>
+
+      
     </>
   );
 }
--- src/server-view/MainPanel.js
+++ src/server-view/MainPanel.js
@@ -1,41 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import './MainPanel.css'
 import SmoothieKingLogo from './logo2.png'
 import SearchBar from './SearchBar'
 import SideBar from './SideBar'
-import { getProductsByName } from '../databaseConnections/sharedFunctions'
-import ProductCard from './ProductCard'
-import {CheckDisplay} from '../customer-view/CustomerView';
 
-function showProducts(productName, setProducts) {
-    console.log("Showing products");
-    getProductsByName(productName).then(res => {
-        console.log(res);
-        setProducts(res);
-    });
-}
 
 export default function MainPanel() {
-    const [products, setProducts] = useState([]);
-
     return (
         // <>
             // <div id="allContent">
                 <div id="mainPanel">
                     <img id="logo" alt="Smoothie King Logo" src={SmoothieKingLogo}></img>
-                    <SearchBar showProducts={(productName) => {showProducts(productName, setProducts)}}></SearchBar>
-                    <div id="products">
-                        <CheckDisplay dataProp={products} hasSearched={true}/>
-                        {/* {products.map(product => {
-                            return (
-                                <div key={product.id} class="productCardContainer">
-                                    <ProductCard productName={product.name}/>
-                                </div>
-                            )
-                        })} */}
-                    </div>
+                    <SearchBar></SearchBar>
+                    
+
+                </div>
+
             // </div>
+            
         // </>
+
     )
 }
 
