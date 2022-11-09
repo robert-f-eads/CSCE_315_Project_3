20349b10f0a43f171fea3fb91bc2c06f86bcf5a9
shreyesk <sshreyes.kaliyur@gmail.com>
2022-11-08 22:48:32 -0600

    add products to view after search

--- src/server-view/MainPanel.js
+++ src/server-view/MainPanel.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './MainPanel.css'
 import SmoothieKingLogo from './logo2.png'
 import SearchBar from './SearchBar'
@@ -6,26 +6,31 @@ import SideBar from './SideBar'
 import { getProductsByName } from '../databaseConnections/sharedFunctions'
 import ProductCard from './ProductCard'
 
-function showProducts(productName) {
+function showProducts(productName, setProducts) {
     console.log("Showing products");
     getProductsByName(productName).then(res => {
         console.log(res);
+        setProducts(res);
     });
 }
 
 export default function MainPanel() {
+    const [products, setProducts] = useState([]);
+
     return (
         // <>
             // <div id="allContent">
                 <div id="mainPanel">
                     <img id="logo" alt="Smoothie King Logo" src={SmoothieKingLogo}></img>
-                    <SearchBar showProducts={showProducts}></SearchBar>
+                    <SearchBar showProducts={(productName) => {showProducts(productName, setProducts)}}></SearchBar>
                     <div id="products">
-                        <ProductCard productName = {"Strawberry Kiwi Breeze"}></ProductCard>
-                        <ProductCard productName = {"Strawberry Kiwi Breeze"}></ProductCard>
-                        <ProductCard productName = {"Strawberry Kiwi Breeze"}></ProductCard>
-                        <ProductCard productName = {"Strawberry Kiwi Breeze"}></ProductCard>
-
+                        {products.map(product => {
+                            return (
+                                <div key={product.id} class="productCardContainer">
+                                    <ProductCard productName={product.name}/>
+                                </div>
+                            )
+                        })}
                     </div>
             // </div>
         // </>
