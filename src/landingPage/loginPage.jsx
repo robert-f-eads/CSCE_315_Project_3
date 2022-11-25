77d092ae553931c7bdd8f82129f7acda4f17fda8
Robert Eads <robert.f.eads@tamu.edu>
2022-11-25 10:07:56 -0600

    Finished login and started sign up page

--- src/landingPage/loginPage.jsx
+++ src/landingPage/loginPage.jsx
@@ -3,33 +3,101 @@ import Navbar from "../sharedComponets/Navbar/navbar"
 import Footer from "../sharedComponets/Footer/footer"
 import adv3 from '../assets/advertisement3.jpg'
 import Logo from '../assets/Logo.png'
+import {loginCustomer, loginEmployee} from '../databaseConnections/databaseFunctionExports'
+import { useState } from 'react'
 
 
+async function handleLogin(loginChooser) {
+    let name = document.getElementById('nameEntryField').value
+    let id = document.getElementById('idEntryField').value
+
+    //Change back to black border on successful entry
+    if(document.getElementById('nameEntryField').style.borderColor === "red") {document.getElementById('nameEntryField').style.borderColor = "black"}
+    if(document.getElementById('idEntryField').style.borderColor === "red") {document.getElementById('idEntryField').style.borderColor = "black"}
+    
+    //Make border red if missed entry
+    if(name === "") {document.getElementById('nameEntryField').style.borderColor = "red"; return}
+    if(id === "") {document.getElementById('idEntryField').style.borderColor = "red"; return}
+       
+    if(loginChooser) {
+        //Call the login function
+        let response = await (loginCustomer(id, name))
+
+        //Display error if not valid
+        if(response === false) {document.getElementById('notFoundError').classList.remove("showOnError"); return}
+
+        //Give this data to the customer view
+        alert(`Welcome ${response[0].firstname} ${response[0].lastname} (id: ${response[0].id})`)
+    }
+    else {
+        //Call the login function
+        let response = await (loginEmployee(id, name))
+        //Display error if not valid
+        if(response === false) {document.getElementById('notFoundError').classList.remove("showOnError"); return}
+
+        //Give this data to the server/manager view
+        alert(`Welcome ${response[0].firstname} ${response[0].lastname} (id: ${response[0].id}, admin: ${response[0].isadmin})`)
+    }
+
+    //Reset to default on sucessfuly entry
+    document.getElementById('notFoundError').classList.add("showOnError")
+    document.getElementById('nameEntryField').value = ""
+    document.getElementById('idEntryField').value = ""
+}
+
+function handleLoginChange(value) {
+    if(value) {document.getElementById('signup').classList.add("showOnError")}
+    else {document.getElementById('signup').classList.remove("showOnError")}
+}
 
 const LoginPage = () => {
+    const [loginChooser, setloginChooser] = useState(true)
+    const [viewChooser, setviewChooser] = useState(true)
 
     return <>
         <Navbar display={false}/>
         <div className="mainBody">
             <div className='Login-wrapper'>
                 <img id='logo' src={Logo} alt='Logo'/>
-                <div className="mainBody_Login">
+
+                {/* Customer/Employee Login */}
+                {viewChooser && <div className="mainBody_Login">
                     <div className='mainBody_Login-center'>
                         <label>First Name</label>
-                        <input type="text" placeholder="John"/>
-                        <label>Rewards ID</label>
-                        <input type="text" placeholder="23"/>
-                        <div id="buttonHolder"><button onClick={() => {alert("Login")}}>Log In</button></div>
+                        <input id="nameEntryField" type="text" placeholder="John"/>
+                        {!loginChooser && <label>Employee ID</label>}
+                        {loginChooser && <label>Rewards ID</label>}
+                        <input id="idEntryField" type="text" placeholder="23"/>
+                        <p id="notFoundError" className='showOnError'>*No user found</p>
+                        <div id="buttonHolder"><button onClick={() => {handleLogin(loginChooser)}}>Log In</button></div>
                     </div>
-                    <div className='signup-wrapper'>
-                        <p>Don't have an account?</p>
-                        <p><a>Click to get started</a></p>
+                    <div className='bottom-wrapper'>
+                        <div id="signup" className='signup-wrapper'>
+                            <p>Don't have an account?</p>
+                            <button onClick={() => {setviewChooser(false)}}>Click to get started</button>
+                        </div>
+                        <div className='employee-wrapper'>
+                            {loginChooser && <button onClick={() => {setloginChooser(false); handleLoginChange(loginChooser)}}>Employee Login</button>}
+                            {!loginChooser && <button onClick={() => {setloginChooser(true); handleLoginChange(loginChooser)}}>Customer Login</button>}
+                        </div>
                     </div>
-                    <div className='employee-wrapper'>
-                        <a>Employee Login</a> {/*This just changes what the sign in button does*/}
-                    </div>
-                
-                </div>
+                </div>}
+
+                {/* Customer Sign up */}
+                {!viewChooser && <div className='mainBody_Signup'>
+                    <button onClick={() => {setviewChooser(true)}}>Button</button>
+                    <label>First Name</label>
+                    <input id="firstNameEntryField" type="text" placeholder="John"/>
+                    <label>Last Name</label>
+                    <input id="lastNameEntryField" type="text" placeholder="Smith"/>
+                    <label>Email</label>
+                    <input id="emailEntryField" type="text" placeholder="example@mail.com"/>
+                    <label>Phone Number</label>
+                    <input id="phoneNumberEntryField" type="text" placeholder="XXX-XXX-XXXX"/>
+                    <label>Birthday</label>
+                    <input id="birthdayEntryField" type="text" placeholder=""/>
+                    <button>Sign Up</button>
+                </div>}
             </div>
             
             <div className="mainBody_Pic">
